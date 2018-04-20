672a5a56b5a27bdf86976f6dec0e1b60cce68949
Calvin Tong <calvinytong@gmail.com>
2018-04-19 22:37:52 -0400

    add inital setup scripts

---
+++ download_data.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+DATA_PATH=$1
+
+if [ -z "$1" ]
+  then
+    DATA_PATH='.'
+fi
+
+
+# Download to ./DATA_PATH
+kaggle competitions download -c dstl-satellite-imagery-feature-detection -p $DATA_PATH
+
+cd $DATA_PATH
+
+# Unzip recursivly
+find . -name "*.zip" | xargs -P 5 -I fileName sh -c 'unzip -o -d "$(dirname "fileName")/$(basename -s .zip "fileName")" "fileName"'
+
+# Delete files
+find . -name "*.zip" -type f -delete
+
+
+
